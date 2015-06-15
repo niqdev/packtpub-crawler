ba4ee3601127f0af7e11753794225c6d4dee7bd2
niqdev <niqdev@gmail.com>
2015-06-15 07:28:07 +0200

    config file environment

--- script/spider.py
+++ script/spider.py
@@ -1,4 +1,4 @@
-"""
+'''
 // setup environment
 sudo easy_install pip
 
@@ -8,21 +8,48 @@ pip freeze
 sudo pip install termcolor
 sudo pip install beautifulsoup4
 sudo pip install requests
-"""
+
+// run
+python spider.py
+python spider.py -h
+'''
 
 import argparse
+import ConfigParser
 from utils import *
 
-if __name__ == '__main__':
+def do_login():
+    login_soup = make_soup(BASE_DEV_URL + LOGIN_DEV_URL, DELAY_REQUEST)
 
-    parser = argparse.ArgumentParser(description="Download FREE eBook every day from www.packtpub.com", version='0.1')
+def parse_environment(param):
+    """
+    Parse environment: default is development
+    """
+    environment = 'config/dev.cfg'
+
+    if param and param.strip() == 'prod':
+        environment = 'config/prod.cfg'
+
+    log_info('[*] init environment: ' + environment)
+    
+    config = ConfigParser.ConfigParser()
+    return config.read(environment)
+
+def main():
+    parser = argparse.ArgumentParser(description='Download FREE eBook every day from www.packtpub.com', version='0.1')
+    parser.add_argument('-e', '--environment', dest='environment', default='dev', help='configuration environment')
     args = parser.parse_args()
 
     try:
-        current_ip_address()
+        config = parse_environment(args.environment)
+        #current_ip_address()
+        #do_login()
 
     except KeyboardInterrupt:
-        log_error("[-] interrupted manually")
+        log_error('[-] interrupted manually')
     except Exception as e:
         log_warn('[-] {0}'.format(e))
-        log_error("[-] something weird occurred, exiting...")
+        log_error('[-] something weird occurred, exiting...')
+
+if __name__ == '__main__':
+    main()
--- script/utils.py
+++ script/utils.py
@@ -1,17 +1,18 @@
 from termcolor import cprint
 import json
-
 import requests
 from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup
+from time import sleep
 
 def log_error(message):
-    cprint(message, "red")
+    cprint(message, 'red')
 def log_warn(message):
-    cprint(message, "yellow")
+    cprint(message, 'yellow')
 def log_info(message):
-    cprint(message, "cyan")
+    cprint(message, 'cyan')
 def log_success(message):
-    cprint(message, "green")
+    cprint(message, 'green')
 def log_json(list_dict):
     print json.dumps(list_dict, indent=2)
 
@@ -22,9 +23,36 @@ def current_ip_address():
 
     try:
         response = requests.get('http://www.ip-addr.es')
-        print "[*] fetching URL... {0} | {1}".format(response.status_code, response.url)
-        log_success("[+] your current ip address is: {0}".format(response.text.strip()))
+        print '[*] fetching URL... {0} | {1}'.format(response.status_code, response.url)
+        log_success('[+] your current ip address is: {0}'.format(response.text.strip()))
     except ConnectionError, e:
         print '[-] {0}'.format(e)
         log_error('[-] error internet connection, exiting...')
         exit(0)
+
+def make_soup(url, delay):
+    """
+    Makes soup from url
+    """
+
+    soup = None
+    try:
+        headers = {
+            #latest version of chrome
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+        }
+        response = requests.get(url, headers=headers)
+
+        print '[*] fetching url... {0} | {1}'.format(response.status_code, response.url)
+        soup = BeautifulSoup(response.text, from_encoding=response.encoding)
+        #print soup.prettify().encode('utf-8')
+        sleep(delay)
+
+    except ConnectionError, e:
+        log_warn('[-] {0}'.format(e))
+
+    if soup is None:
+        log_error('[-] error while making soup, exiting...')
+        exit(0)
+    else:
+        return soup
