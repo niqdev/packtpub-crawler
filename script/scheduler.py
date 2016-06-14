8bcae09272e475c96d91b1c2a2fd32ba0ab69f65
niqdev <niqdev@gmail.com>
2016-06-14 10:04:16 +0200

    update heroku scheduler

--- script/scheduler.py
+++ script/scheduler.py
@@ -4,12 +4,9 @@ import shutil
 
 sched = BlockingScheduler()
 
-#@sched.scheduled_job('interval', minutes=5)
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=10)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=9)
 def scheduled_job():
     print('New job: packtpub-crawler')
-    #print(os.listdir(os.curdir))
     shutil.rmtree('./ebooks', ignore_errors=True)
     os.system('python script/spider.py --config config/prod.cfg --upload drive --notify')
 
